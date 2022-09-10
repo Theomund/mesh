a760a761031d575bd342f7a343d82f44689312e4
Theomund <gregoryshuck96@gmail.com>
2022-09-10 09:23:31 -0400

    Add work-in-progress login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container maxW="lg" py="24" px="8">
+      <Stack textAlign="center" spacing="8">
+        <Stack>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <Heading>Mesh</Heading>
+        </Stack>
+        <Box
+          py="8"
+          px="10"
+          bg="bg-surface"
+          boxShadow={useColorModeValue('md', 'md-dark')}
+          borderRadius="xl"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Stack spacing="8">
+            <Stack>
+              <FormControl>
+                <FormLabel htmlFor="username">Username</FormLabel>
+                <Input id="username" type="username"/>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input id="password" type="password"/>
+              </FormControl>
+            </Stack>
+            <Button colorScheme="blue">Login</Button>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
