e14d478773fafa4799c12d5a7a70b7bf326088a6
Theomund <gregoryshuck96@gmail.com>
2022-09-10 10:51:20 -0400

    Add icons to user login fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,17 @@ import {
     FormControl,
     FormLabel,
     Heading,
+    IconButton,
     Input,
+    InputGroup,
+    InputLeftElement,
+    InputRightElement,
     Stack,
     useColorModeValue
 } from "@chakra-ui/react";
 import {GiMeshNetwork} from "react-icons/gi";
 import {IconContext} from "react-icons";
+import {FaEye, FaLock, FaUser} from "react-icons/fa";
 
 function App() {
     return (
@@ -23,16 +28,26 @@ function App() {
                     </IconContext.Provider>
                     <Heading>Mesh</Heading>
                 </Stack>
-                <Box py="8" px="10" bg="bg-surface" boxShadow={useColorModeValue('md', 'md-dark')} borderRadius="xl">
+                <Box py="8" px="10" bg="bg-surface" boxShadow={useColorModeValue("md", "md-dark")} borderRadius="xl">
                     <Stack spacing="8">
                         <Stack>
                             <FormControl>
                                 <FormLabel htmlFor="username">Username</FormLabel>
-                                <Input id="username" type="username"/>
+                                <InputGroup>
+                                    <InputLeftElement pointerEvents="none" children={<FaUser color="#A0AEC0"/>}/>
+                                    <Input id="username" type="username"/>
+                                </InputGroup>
                             </FormControl>
                             <FormControl>
                                 <FormLabel htmlFor="password">Password</FormLabel>
-                                <Input id="password" type="password"/>
+                                <InputGroup>
+                                    <InputLeftElement pointerEvents="none" children={<FaLock color="#A0AEC0"/>}/>
+                                    <Input id="password" type="password"/>
+                                    <InputRightElement>
+                                        <IconButton variant="link" aria-label="Mask password"
+                                                    icon={<FaEye color="#A0AEC0"/>}/>
+                                    </InputRightElement>
+                                </InputGroup>
                             </FormControl>
                         </Stack>
                         <Button colorScheme="blue">Login</Button>
