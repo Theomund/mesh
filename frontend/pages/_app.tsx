679a9fff5497aabd222d171705a25057878239cf
Theomund <gregoryshuck96@gmail.com>
2022-09-11 07:29:47 -0400

    Reinstate login page

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/inter';
+
+const customTheme = extendTheme({
+  fonts: {
+    body: "Inter",
+    heading: "Inter",
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
